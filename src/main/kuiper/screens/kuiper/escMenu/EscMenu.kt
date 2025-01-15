6359b5b267b940245a7ebb4d9ccef0d8c665da69
Liam <liamjdavison@gmail.com>
2025-01-15 21:36:28 +0000

    Final push

--- src/main/kuiper/screens/kuiper/escMenu/EscMenu.kt
+++ src/main/kuiper/screens/kuiper/escMenu/EscMenu.kt
@@ -12,41 +12,41 @@ import screens.Kuiper.KuiperGame
 @RegisterClass
 class EscMenu : Control() {
 
-    @RegisterSignal
-    val resumeSignal: Signal0 by signal0()
+	@RegisterSignal
+	val resumeSignal: Signal0 by signal0()
 
-    @RegisterSignal
-    val quitSignal: Signal0 by signal0()
+	@RegisterSignal
+	val quitSignal: Signal0 by signal0()
 
-    // Called when the node enters the scene tree for the first time.
-    @RegisterFunction
-    override fun _ready() {
+	// Called when the node enters the scene tree for the first time.
+	@RegisterFunction
+	override fun _ready() {
 
-    }
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    @RegisterFunction
-    override fun _process(delta: Double) {
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	@RegisterFunction
+	override fun _process(delta: Double) {
 
-    }
+	}
 
-    @RegisterFunction
-    override fun _input(event: InputEvent?) {
+	@RegisterFunction
+	override fun _input(event: InputEvent?) {
 
-    }
+	}
 
-    /**
-     * Resume the game by closing the escape menu
-     */
-    @RegisterFunction
-    fun _on_resume() {
-        // this looks naughty!
-        (getParent() as KuiperGame).escMenuVisible = false
-    }
+	/**
+	 * Resume the game by closing the escape menu
+	 */
+	@RegisterFunction
+	fun _on_resume() {
+		// this looks naughty!
+		(getParent() as KuiperGame).escMenuVisible = false
+	}
 
-    @RegisterFunction
-    fun _on_quit() {
-        // check if there is an unsaved game in progress, or some other such sanity check
-        getTree()?.quit()
-    }
+	@RegisterFunction
+	fun _on_quit() {
+		// check if there is an unsaved game in progress, or some other such sanity check
+		getTree()?.quit()
+	}
 }
