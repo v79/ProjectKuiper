18b600fc9e3e24e97bd021df33915bb8dc2e160c
Liam <liamjdavison@gmail.com>
2025-02-04 20:57:52 +0000

    Maps are beyond me now so I am just going for a simple grid of hexes. Maybe I can add some visual distinctions over time. I anticipate that the game will start with just one or active hexes and others can be unlocked.
    Will have to drop adjacency bonus ideas for now?

---
+++ src/main/kuiper/hexgrid/HexGrid.kt
@@ -0,0 +1,19 @@
+package hexgrid
+
+import godot.Control
+import godot.annotation.RegisterClass
+import godot.annotation.RegisterFunction
+
+@RegisterClass
+class HexGrid : Control() {
+
+	@RegisterFunction
+	override fun _ready() {
+
+	}
+
+	@RegisterFunction
+	override fun _process(delta: Double) {
+
+	}
+}
