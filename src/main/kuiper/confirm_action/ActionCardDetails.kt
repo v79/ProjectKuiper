d42a8459b11ed89a45aa4e9ca68f13102b55a49c
Liam <liamjdavison@gmail.com>
2025-02-17 16:53:29 +0000

    Some placeholder icons from SVGs

--- src/main/kuiper/confirm_action/ActionCardDetails.kt
+++ src/main/kuiper/confirm_action/ActionCardDetails.kt
@@ -20,90 +20,90 @@ import state.Building
 @RegisterClass
 class ActionCardDetails : Control() {
 
-    // Globals
-    private lateinit var signalBus: SignalBus
+	// Globals
+	private lateinit var signalBus: SignalBus
 
-    @RegisterProperty
-    @Export
-    var cardTitle: String = ""
+	@RegisterProperty
+	@Export
+	var cardTitle: String = ""
 
-    // UI elements
-    private val panelContainer: PanelContainer by lazy { getNodeAs("PanelContainer")!! }
-    private val titleLabel: Label by lazy { getNodeAs("%CardTitle")!! }
-    private val turnsLabel: Label by lazy { getNodeAs("%Turns")!! }
-    private val goldLabel: Label by lazy { getNodeAs("%GoldCost")!! }
-    private val influenceLabel: Label by lazy { getNodeAs("%InfluenceCost")!! }
-    private val conMatsLabel: Label by lazy { getNodeAs("%ConMatsCost")!! }
-    private val descLabel: Label by lazy { getNodeAs("%CardDescription")!! }
-    private val sectorSizeLabel: Label by lazy { getNodeAs("%SectorSize")!! }
+	// UI elements
+	private val panelContainer: PanelContainer by lazy { getNodeAs("PanelContainer")!! }
+	private val titleLabel: Label by lazy { getNodeAs("%CardTitle")!! }
+	private val turnsLabel: Label by lazy { getNodeAs("%Turns")!! }
+	private val goldLabel: Label by lazy { getNodeAs("%GoldCost")!! }
+	private val influenceLabel: Label by lazy { getNodeAs("%InfluenceCost")!! }
+	private val conMatsLabel: Label by lazy { getNodeAs("%ConMatsCost")!! }
+	private val descLabel: Label by lazy { getNodeAs("%CardDescription")!! }
+	private val sectorSizeLabel: Label by lazy { getNodeAs("%SectorSize")!! }
 
 
-    @RegisterFunction
-    override fun _ready() {
-        signalBus = getNodeAs("/root/SignalBus")!!
+	@RegisterFunction
+	override fun _ready() {
+		signalBus = getNodeAs("/root/SignalBus")!!
 
-        signalBus.showActionConfirmation.connect { _, c ->
-            updateCard(c)
-        }
-    }
+		signalBus.showActionConfirmation.connect { _, c ->
+			updateCard(c)
+		}
+	}
 
-    @RegisterFunction
-    fun updateCard(card: ActionCard) {
-        titleLabel.text = card.cardName
-        card.action?.let { action ->
-        val building: Building? = action.buildingToConstruct
-            // set the theme variation to display the correct texture for the card
-            when(action.type) {
-                ActionType.BUILD -> {
-                    panelContainer.setThemeTypeVariation("BuildCard".asStringName())
-                    sectorSizeLabel.text = building?.sectors.toString()
-                }
-                ActionType.INVEST -> {
-                    panelContainer.setThemeTypeVariation("InvestCard".asStringName())
-                }
-                else -> {
-                    panelContainer.setThemeTypeVariation("PanelContainer".asStringName())
-                }
-            }
+	@RegisterFunction
+	fun updateCard(card: ActionCard) {
+		titleLabel.text = card.cardName
+		card.action?.let { action ->
+		val building: Building? = action.buildingToConstruct
+			// set the theme variation to display the correct texture for the card
+			when(action.type) {
+				ActionType.BUILD -> {
+					panelContainer.setThemeTypeVariation("BuildCard".asStringName())
+					sectorSizeLabel.text = building?.sectors.toString()
+				}
+				ActionType.INVEST -> {
+					panelContainer.setThemeTypeVariation("InvestCard".asStringName())
+				}
+				else -> {
+					panelContainer.setThemeTypeVariation("PanelContainer".asStringName())
+				}
+			}
 
-            turnsLabel.text = action.turns.toString()
-            descLabel.text = action.description
-            val sBuilder = StringBuilder()
-            val goldCost = action.getCost(ResourceType.GOLD)
-            buildCostString(goldCost, sBuilder)
-            goldLabel.text = sBuilder.toString()
-            sBuilder.clear()
-            val infCost = action.getCost(ResourceType.INFLUENCE)
-            buildCostString(infCost, sBuilder)
-            influenceLabel.text = sBuilder.toString()
-            sBuilder.clear()
-            val conMatsCost = action.getCost(ResourceType.CONSTRUCTION_MATERIALS)
-            buildCostString(conMatsCost, sBuilder)
-            conMatsLabel.text = sBuilder.toString()
-            sectorSizeLabel.visible = action.type == ActionType.BUILD
-        }
-    }
+			turnsLabel.text = action.turns.toString()
+			descLabel.text = action.description
+			val sBuilder = StringBuilder()
+			val goldCost = action.getCost(ResourceType.GOLD)
+			buildCostString(goldCost, sBuilder)
+			goldLabel.text = sBuilder.toString()
+			sBuilder.clear()
+			val infCost = action.getCost(ResourceType.INFLUENCE)
+			buildCostString(infCost, sBuilder)
+			influenceLabel.text = sBuilder.toString()
+			sBuilder.clear()
+			val conMatsCost = action.getCost(ResourceType.CONSTRUCTION_MATERIALS)
+			buildCostString(conMatsCost, sBuilder)
+			conMatsLabel.text = sBuilder.toString()
+			sectorSizeLabel.visible = action.type == ActionType.BUILD
+		}
+	}
 
-    /**
-     * Build a string representation of the cost of an action
-     * @param resourceCost a pair of integers representing the cost of the action
-     * @param sBuilder a StringBuilder to build the string
-     */
-    private fun buildCostString(resourceCost: Pair<Int?, Int?>, sBuilder: StringBuilder) {
-        resourceCost.let { cost ->
-            if (cost.first != null) {
-                sBuilder.append(cost.first.toString())
-            }
-            if (cost.second != null) {
-                if (cost.first != null) {
-                    sBuilder.appendLine(" + ")
-                }
-                sBuilder.append("${cost.second.toString()}/turn")
-            }
-        }
-    }
+	/**
+	 * Build a string representation of the cost of an action
+	 * @param resourceCost a pair of integers representing the cost of the action
+	 * @param sBuilder a StringBuilder to build the string
+	 */
+	private fun buildCostString(resourceCost: Pair<Int?, Int?>, sBuilder: StringBuilder) {
+		resourceCost.let { cost ->
+			if (cost.first != null) {
+				sBuilder.append(cost.first.toString())
+			}
+			if (cost.second != null) {
+				if (cost.first != null) {
+					sBuilder.appendLine(" + ")
+				}
+				sBuilder.append("${cost.second.toString()}/turn")
+			}
+		}
+	}
 
-    @RegisterFunction
-    override fun _process(delta: Double) {
-    }
+	@RegisterFunction
+	override fun _process(delta: Double) {
+	}
 }
--- src/main/kuiper/confirm_action/ConfirmAction.kt
+++ src/main/kuiper/confirm_action/ConfirmAction.kt
@@ -27,244 +27,244 @@ import utils.hasChildren
 @RegisterClass
 class ConfirmAction : Control() {
 
-    // Globals
-    private lateinit var signalBus: SignalBus
+	// Globals
+	private lateinit var signalBus: SignalBus
 
-    lateinit var hex: Hex
-    lateinit var card: ActionCard
-    lateinit var location: Location
+	lateinit var hex: Hex
+	lateinit var card: ActionCard
+	lateinit var location: Location
 
-    // UI elements
-    private lateinit var titleLabel: Label
-    private lateinit var animationPlayer: AnimationPlayer
-    private lateinit var actionCardDetails: ActionCardDetails
-    private lateinit var costsListContainer: VBoxContainer
-    private lateinit var costsPerTurnContainer: VBoxContainer
-    private lateinit var benefitsListContainer: VBoxContainer
-    private lateinit var buildingListContainer: VBoxContainer
-    private lateinit var buildingHeading: Label
-    private lateinit var hexBoxContainer: CenterContainer
-    private lateinit var hexLocationLabel: Label
-    private lateinit var confirmButton: Button
-    private lateinit var chooseSectorsContainer: HBoxContainer
+	// UI elements
+	private lateinit var titleLabel: Label
+	private lateinit var animationPlayer: AnimationPlayer
+	private lateinit var actionCardDetails: ActionCardDetails
+	private lateinit var costsListContainer: VBoxContainer
+	private lateinit var costsPerTurnContainer: VBoxContainer
+	private lateinit var benefitsListContainer: VBoxContainer
+	private lateinit var buildingListContainer: VBoxContainer
+	private lateinit var buildingHeading: Label
+	private lateinit var hexBoxContainer: CenterContainer
+	private lateinit var hexLocationLabel: Label
+	private lateinit var confirmButton: Button
+	private lateinit var chooseSectorsContainer: HBoxContainer
 
-    // Packed scenes
-    private val hexScene = ResourceLoader.load("res://src/main/kuiper/hexgrid/Hex.tscn") as PackedScene
+	// Packed scenes
+	private val hexScene = ResourceLoader.load("res://src/main/kuiper/hexgrid/Hex.tscn") as PackedScene
 
-    private var confirmEnabled = true
+	private var confirmEnabled = true
 
-    @RegisterFunction
-    override fun _ready() {
-        hide()
-        signalBus = getNodeAs("/root/SignalBus")!!
-        titleLabel = getNodeAs("%ConfirmActionTitle")!!
-        animationPlayer = getNodeAs("AnimationPlayer")!!
-        actionCardDetails = getNodeAs("%ActionCardDetails")!!
-        costsListContainer = getNodeAs("%CostsList")!!
-        benefitsListContainer = getNodeAs("%BenefitsList")!!
-        costsPerTurnContainer = getNodeAs("%CostsPerTurnList")!!
-        buildingListContainer = getNodeAs("%BuildingList")!!
-        buildingHeading = getNodeAs("%Building_")!!
-        hexBoxContainer = getNodeAs("%HexBoxContainer")!!
-        hexLocationLabel = getNodeAs("%HexLocationLabel")!!
-        confirmButton = getNodeAs("%ConfirmButton")!!
-        chooseSectorsContainer = getNodeAs("%ConfirmSectorContainer")!!
-        chooseSectorsContainer.visible = false
+	@RegisterFunction
+	override fun _ready() {
+		hide()
+		signalBus = getNodeAs("/root/SignalBus")!!
+		titleLabel = getNodeAs("%ConfirmActionTitle")!!
+		animationPlayer = getNodeAs("AnimationPlayer")!!
+		actionCardDetails = getNodeAs("%ActionCardDetails")!!
+		costsListContainer = getNodeAs("%CostsList")!!
+		benefitsListContainer = getNodeAs("%BenefitsList")!!
+		costsPerTurnContainer = getNodeAs("%CostsPerTurnList")!!
+		buildingListContainer = getNodeAs("%BuildingList")!!
+		buildingHeading = getNodeAs("%Building_")!!
+		hexBoxContainer = getNodeAs("%HexBoxContainer")!!
+		hexLocationLabel = getNodeAs("%HexLocationLabel")!!
+		confirmButton = getNodeAs("%ConfirmButton")!!
+		chooseSectorsContainer = getNodeAs("%ConfirmSectorContainer")!!
+		chooseSectorsContainer.visible = false
 
-        signalBus.showActionConfirmation.connect { h, c ->
-            hex = h
-            card = c
-            location = h.location
-            updateUI()
-        }
-    }
+		signalBus.showActionConfirmation.connect { h, c ->
+			hex = h
+			card = c
+			location = h.location
+			updateUI()
+		}
+	}
 
-    @RegisterFunction
-    fun updateUI() {
-        resetUI()
-        renderHex(hex, location)
-        titleLabel.text = "Play ${card.cardName}?"
-        card.action?.let { action ->
-            actionCardDetails.updateCard(card)
-            action.initialCosts.forEach { (resourceType, amount) ->
-                val costLabel = Label()
-                costLabel.setName("CostLabel_${resourceType.name}")
-                costLabel.text = "  $amount ${resourceType.displayName}"
-                costsListContainer.addChild(costLabel)
-            }
-            ResourceType.entries.forEach { resourceType ->
-                val cost = action.getCost(resourceType)
-                if (cost.second != null) {
-                    val costLabel = Label()
-                    costLabel.setName("CPTLabel_${resourceType.name}")
-                    costLabel.text = "  ${cost.second} ${resourceType.displayName}"
-                    costsPerTurnContainer.addChild(costLabel)
-                }
-                val benefits = action.getBenefits(resourceType)
-                benefits.let { (perTurn, completion) ->
-                    val benefitLabel = Label()
-                    benefitLabel.setName("BenefitLabel_${resourceType.name}")
-                    benefitLabel.autowrapMode = TextServer.AutowrapMode.AUTOWRAP_WORD
-                    val sBuilder = StringBuilder()
-                    if (perTurn != null && perTurn > 0) {
-                        sBuilder.append("  $perTurn ${resourceType.displayName} per turn")
-                    }
-                    if (completion != null) {
-                        if (sBuilder.isNotEmpty()) {
-                            sBuilder.append(" and ")
-                        }
-                        sBuilder.append("set ${resourceType.displayName} to $completion when completed")
-                    }
-                    if (sBuilder.isNotEmpty()) {
-                        benefitLabel.text = sBuilder.toString()
-                        benefitsListContainer.addChild(benefitLabel)
-                    }
-                }
-            }
-            Science.entries.forEach { science ->
-                val benefit = action.getScienceBenefit(science)
-                if (benefit != null && benefit > 0f) {
-                    val benefitLabel = Label()
-                    benefitLabel.setName("ScienceBenefitLabel_${science.name}")
-                    benefitLabel.text = "  $benefit ${science.displayName} per turn"
-                    benefitsListContainer.addChild(benefitLabel)
-                }
-            }
-            if (costsPerTurnContainer.getChildCount() == 0) {
-                val costLabel = Label()
-                costLabel.setName("CostLabel_NONE")
-                costLabel.text = "  -- None --" // Would be nice if this were in italics
-                costsPerTurnContainer.addChild(costLabel)
-            }
-            if (action.type == ActionType.BUILD) {
-                if (action.buildingToConstruct == null) {
-                    GD.printErr("A Build action must have a building to construct: ${action.id}->${action.actionName}")
-                } else {
+	@RegisterFunction
+	fun updateUI() {
+		resetUI()
+		renderHex(hex, location)
+		titleLabel.text = "Play ${card.cardName}?"
+		card.action?.let { action ->
+			actionCardDetails.updateCard(card)
+			action.initialCosts.forEach { (resourceType, amount) ->
+				val costLabel = Label()
+				costLabel.setName("CostLabel_${resourceType.name}")
+				costLabel.text = "  $amount ${resourceType.displayName}"
+				costsListContainer.addChild(costLabel)
+			}
+			ResourceType.entries.forEach { resourceType ->
+				val cost = action.getCost(resourceType)
+				if (cost.second != null) {
+					val costLabel = Label()
+					costLabel.setName("CPTLabel_${resourceType.name}")
+					costLabel.text = "  ${cost.second} ${resourceType.displayName}"
+					costsPerTurnContainer.addChild(costLabel)
+				}
+				val benefits = action.getBenefits(resourceType)
+				benefits.let { (perTurn, completion) ->
+					val benefitLabel = Label()
+					benefitLabel.setName("BenefitLabel_${resourceType.name}")
+					benefitLabel.autowrapMode = TextServer.AutowrapMode.AUTOWRAP_WORD
+					val sBuilder = StringBuilder()
+					if (perTurn != null && perTurn > 0) {
+						sBuilder.append("  $perTurn ${resourceType.displayName} per turn")
+					}
+					if (completion != null) {
+						if (sBuilder.isNotEmpty()) {
+							sBuilder.append(" and ")
+						}
+						sBuilder.append("set ${resourceType.displayName} to $completion when completed")
+					}
+					if (sBuilder.isNotEmpty()) {
+						benefitLabel.text = sBuilder.toString()
+						benefitsListContainer.addChild(benefitLabel)
+					}
+				}
+			}
+			Science.entries.forEach { science ->
+				val benefit = action.getScienceBenefit(science)
+				if (benefit != null && benefit > 0f) {
+					val benefitLabel = Label()
+					benefitLabel.setName("ScienceBenefitLabel_${science.name}")
+					benefitLabel.text = "  $benefit ${science.displayName} per turn"
+					benefitsListContainer.addChild(benefitLabel)
+				}
+			}
+			if (costsPerTurnContainer.getChildCount() == 0) {
+				val costLabel = Label()
+				costLabel.setName("CostLabel_NONE")
+				costLabel.text = "  -- None --" // Would be nice if this were in italics
+				costsPerTurnContainer.addChild(costLabel)
+			}
+			if (action.type == ActionType.BUILD) {
+				if (action.buildingToConstruct == null) {
+					GD.printErr("A Build action must have a building to construct: ${action.id}->${action.actionName}")
+				} else {
 
-                    // Valid building, so show the building details
-                    confirmEnabled = false
-                    val buildingLabel = Label()
-                    chooseSectorsContainer.visible = true
+					// Valid building, so show the building details
+					confirmEnabled = false
+					val buildingLabel = Label()
+					chooseSectorsContainer.visible = true
 
 
-                    action.buildingToConstruct?.let b@{ building ->
-                        when (building) {
-                            is Building.HQ -> {
-                                GD.printErr("Cannot build an HQ, should already exist! ${action.id}->${action.actionName}")
-                                return@b
-                            }
+					action.buildingToConstruct?.let b@{ building ->
+						when (building) {
+							is Building.HQ -> {
+								GD.printErr("Cannot build an HQ, should already exist! ${action.id}->${action.actionName}")
+								return@b
+							}
 
-                            is Building.ScienceLab -> {
-                                buildingLabel.setName("Build_ScienceLab_${building.labName}")
-                                buildingLabel.text = "  New ${building.labName} at ${hex.location.name}"
-                                buildingListContainer.addChild(buildingLabel)
-                                building.baseRunningCost.let { (resourceType, amount) ->
-                                    val costLabel = Label()
-                                    costLabel.setName("Build_ScienceLab_Cost_${building.labName}")
-                                    costLabel.text = "  Costing $amount ${resourceType.displayName} per turn"
-                                    buildingListContainer.addChild(costLabel)
-                                }
-                                building.sciencesProduced.forEach { (science, amount) ->
-                                    val scienceLabel = Label()
-                                    scienceLabel.setName("Build_ScienceLab_Science_${science.name}")
-                                    scienceLabel.text = "  Producing $amount ${science.displayName} per turn"
-                                    buildingListContainer.addChild(scienceLabel)
-                                }
-                            }
-                        }
-                        val sectorCountLabel = Label()
-                        sectorCountLabel.setName("SectorCountLabel_${building.sectors}")
-                        val contiguous = if (building.sectorsMustBeContiguous) " contiguous" else ""
-                        val s = if(building.sectors == 1) "" else "s"
-                        sectorCountLabel.text = "  Requires ${building.sectors}${contiguous} sector$s"
-                        buildingListContainer.addChild(sectorCountLabel)
+							is Building.ScienceLab -> {
+								buildingLabel.setName("Build_ScienceLab_${building.labName}")
+								buildingLabel.text = "  New ${building.labName} at ${hex.location.name}"
+								buildingListContainer.addChild(buildingLabel)
+								building.baseRunningCost.let { (resourceType, amount) ->
+									val costLabel = Label()
+									costLabel.setName("Build_ScienceLab_Cost_${building.labName}")
+									costLabel.text = "  Costing $amount ${resourceType.displayName} per turn"
+									buildingListContainer.addChild(costLabel)
+								}
+								building.sciencesProduced.forEach { (science, amount) ->
+									val scienceLabel = Label()
+									scienceLabel.setName("Build_ScienceLab_Science_${science.name}")
+									scienceLabel.text = "  Producing $amount ${science.displayName} per turn"
+									buildingListContainer.addChild(scienceLabel)
+								}
+							}
+						}
+						val sectorCountLabel = Label()
+						sectorCountLabel.setName("SectorCountLabel_${building.sectors}")
+						val contiguous = if (building.sectorsMustBeContiguous) " contiguous" else ""
+						val s = if(building.sectors == 1) "" else "s"
+						sectorCountLabel.text = "  Requires ${building.sectors}${contiguous} sector$s"
+						buildingListContainer.addChild(sectorCountLabel)
 
-                        if (buildingListContainer.hasChildren()) {
-                            buildingHeading.text = "Once construction complete:"
-                        } else {
-                            buildingHeading.visible = false
-                        }
-                    }
-                }
-            } else {
-                confirmEnabled = true
-            }
+						if (buildingListContainer.hasChildren()) {
+							buildingHeading.text = "Once construction complete:"
+						} else {
+							buildingHeading.visible = false
+						}
+					}
+				}
+			} else {
+				confirmEnabled = true
+			}
 
-            confirmButton.disabled = !confirmEnabled
-        }
-    }
+			confirmButton.disabled = !confirmEnabled
+		}
+	}
 
-    private fun renderHex(hex: Hex, location: Location) {
-        hexBoxContainer.clearChildren()
-        val hexToRender = hexScene.instantiateAs<Hex>()!!
-        hexToRender.id = hex.id
-        hexToRender.hexUnlocked = true
-        hexToRender.triangles = hex.triangles
-        val dropTarget = hexToRender.getNodeAs<HexDropTarget>("%HexDropTarget")!!
-        dropTarget.setName("ConfirmHexDropTarget")
-        dropTarget.hex = hexToRender
-        dropTarget.fillTriangles.resize(6)
-        dropTarget.drawInternals = true
-        dropTarget.nameSectors = true
-        dropTarget.colour = Color.white
-        hex.marker = dropTarget
-        location.sectors.forEachIndexed { index, sector ->
-            if (sector.status == SectorStatus.BUILT) {
-                hex.triangles = Array(6) { 0 }
-                hex.triangles[index] = 1
-                hex.marker.fillTriangles[index] = true
-            }
-        }
-        hexToRender.setName("ConfirmHex${hex.id}")
-        // hide the location label because I've got better one down below
-        hexToRender.getNodeAs<Label>("LocationLabel")!!.visible = false
+	private fun renderHex(hex: Hex, location: Location) {
+		hexBoxContainer.clearChildren()
+		val hexToRender = hexScene.instantiateAs<Hex>()!!
+		hexToRender.id = hex.id
+		hexToRender.hexUnlocked = true
+		hexToRender.triangles = hex.triangles
+		val dropTarget = hexToRender.getNodeAs<HexDropTarget>("%HexDropTarget")!!
+		dropTarget.setName("ConfirmHexDropTarget")
+		dropTarget.hex = hexToRender
+		dropTarget.fillTriangles.resize(6)
+		dropTarget.drawInternals = true
+		dropTarget.nameSectors = true
+		dropTarget.colour = Color.white
+		hex.marker = dropTarget
+		location.sectors.forEachIndexed { index, sector ->
+			if (sector.status == SectorStatus.BUILT) {
+				hex.triangles = Array(6) { 0 }
+				hex.triangles[index] = 1
+				hex.marker.fillTriangles[index] = true
+			}
+		}
+		hexToRender.setName("ConfirmHex${hex.id}")
+		// hide the location label because I've got better one down below
+		hexToRender.getNodeAs<Label>("LocationLabel")!!.visible = false
 
-        // make it big
-        hexToRender.scaleMutate {
-            x = 2.0
-            y = 2.0
-        }
-        val boxContainer = BoxContainer()
-        boxContainer.setName("ConfirmHex_BoxContainer")
-        boxContainer.addChild(hexToRender)
-        hexBoxContainer.addChild(boxContainer)
-        hexLocationLabel.text = location.name
-    }
+		// make it big
+		hexToRender.scaleMutate {
+			x = 2.0
+			y = 2.0
+		}
+		val boxContainer = BoxContainer()
+		boxContainer.setName("ConfirmHex_BoxContainer")
+		boxContainer.addChild(hexToRender)
+		hexBoxContainer.addChild(boxContainer)
+		hexLocationLabel.text = location.name
+	}
 
-    @RegisterFunction
-    fun resetUI() {
-        titleLabel.text = ""
-        costsListContainer.clearChildren()
-        costsPerTurnContainer.clearChildren()
-        benefitsListContainer.clearChildren()
-        buildingListContainer.clearChildren()
-    }
+	@RegisterFunction
+	fun resetUI() {
+		titleLabel.text = ""
+		costsListContainer.clearChildren()
+		costsPerTurnContainer.clearChildren()
+		benefitsListContainer.clearChildren()
+		buildingListContainer.clearChildren()
+	}
 
-    @RegisterFunction
-    override fun _process(delta: Double) {
+	@RegisterFunction
+	override fun _process(delta: Double) {
 
-    }
+	}
 
-    @RegisterFunction
-    fun fadeIn() {
-        animationPlayer.play("show_panel".asStringName())
-    }
+	@RegisterFunction
+	fun fadeIn() {
+		animationPlayer.play("show_panel".asStringName())
+	}
 
-    @RegisterFunction
-    fun confirmAction() {
-        GD.print("Confirming action ${card.cardName}")
-        hide()
-        signalBus.confirmAction.emit(hex, ActionWrapper(card.action!!))
-    }
+	@RegisterFunction
+	fun confirmAction() {
+		GD.print("Confirming action ${card.cardName}")
+		hide()
+		signalBus.confirmAction.emit(hex, ActionWrapper(card.action!!))
+	}
 
-    @RegisterFunction
-    fun cancelAction() {
-        hide()
-        signalBus.cancelActionConfirmation.emit()
-    }
+	@RegisterFunction
+	fun cancelAction() {
+		hide()
+		signalBus.cancelActionConfirmation.emit()
+	}
 
-    @RegisterFunction
-    fun enableConfirmButton() {
-        confirmButton.disabled = false
-    }
+	@RegisterFunction
+	fun enableConfirmButton() {
+		confirmButton.disabled = false
+	}
 }
