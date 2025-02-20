f1d7f0638918fdf602a0b337a97ca63e6239ad77
Liam <liamjdavison@gmail.com>
2025-02-20 06:48:41 +0000

    Fixed game loading.

--- src/main/kuiper/screens/mainMenu/MainMenu.kt
+++ src/main/kuiper/screens/mainMenu/MainMenu.kt
@@ -18,108 +18,113 @@ import state.GameState
 @RegisterClass
 class MainMenu : Node() {
 
-	@RegisterSignal
-	val quitGameSignal: Signal0 by signal0()
+    @RegisterSignal
+    val quitGameSignal: Signal0 by signal0()
 
-	@RegisterSignal
-	val newGameSignal: Signal0 by signal0()
+    @RegisterSignal
+    val newGameSignal: Signal0 by signal0()
 
-	private lateinit var gameConfig: GameConfig
-	private lateinit var loadButton: Button
-	private lateinit var continueButton: Button
-	private lateinit var loadFileDialog: FileDialog
+    private lateinit var gameConfig: GameConfig
+    private lateinit var loadButton: Button
+    private lateinit var continueButton: Button
+    private lateinit var loadFileDialog: FileDialog
 
-	// Called when the node enters the scene tree for the first time.
-	@RegisterFunction
-	override fun _ready() {
-		loadButton = getNodeAs("VBoxContainer/VBoxContainer/Load")!!
-		continueButton = getNodeAs("VBoxContainer/VBoxContainer/Continue")!!
-		loadFileDialog = getNodeAs("LoadFileDialog")!!
-		GD.print("Main Menu ready - got file dialog: $loadFileDialog")
-		loadFileDialog.let {
-			it.setCurrentPath("user://saves/")
-			it.clearFilters()
-			it.addFilter("*.json","Saved JSON games")
-		}
-		GD.print(loadFileDialog.currentPath)
-		gameConfig = loadConfiguration()
-		if (gameConfig.lastSaveFile != null) {
-			GD.print("Last save file: ${gameConfig.lastSaveFile}")
-			continueButton.let {
-				it.disabled = false
-				it.tooltipText = "Continue game from ${gameConfig.lastSaveFile}"
-			}
-			loadButton.disabled = false
-		} else {
-			GD.print("No save file found")
-		}
-	}
+    // Called when the node enters the scene tree for the first time.
+    @RegisterFunction
+    override fun _ready() {
+        loadButton = getNodeAs("VBoxContainer/VBoxContainer/Load")!!
+        continueButton = getNodeAs("VBoxContainer/VBoxContainer/Continue")!!
+        loadFileDialog = getNodeAs("LoadFileDialog")!!
+        GD.print("Main Menu ready - got file dialog: $loadFileDialog")
+        loadFileDialog.let {
+            it.setCurrentPath("user://saves/")
+            it.clearFilters()
+            it.addFilter("*.json", "Saved JSON games")
+        }
+        GD.print(loadFileDialog.currentPath)
+        gameConfig = loadConfiguration()
+        if (gameConfig.lastSaveFile != null) {
+            GD.print("Last save file: ${gameConfig.lastSaveFile}")
+            continueButton.let {
+                it.disabled = false
+                it.tooltipText = "Continue game from ${gameConfig.lastSaveFile}"
+            }
+            loadButton.disabled = false
+        } else {
+            GD.print("No save file found")
+        }
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	@RegisterFunction
-	override fun _process(delta: Double) {
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    @RegisterFunction
+    override fun _process(delta: Double) {
+    }
 
-	@RegisterFunction
-	fun _on_new_game() {
-		GD.print("Setting up new game")
-		getTree()?.changeSceneToFile("res://src/main/kuiper/screens/gameSetup/game_setup.tscn")
-	}
+    @RegisterFunction
+    fun _on_new_game() {
+        GD.print("Setting up new game")
+        getTree()?.changeSceneToFile("res://src/main/kuiper/screens/gameSetup/game_setup.tscn")
+    }
 
-	/**
-	 * Quit the game
-	 * **signal**: quitGameSignal
-	 */
-	@RegisterFunction
-	fun _on_quit_game() {
-		GD.print("Quitting game")
-		getTree()?.quit()
-	}
+    /**
+     * Quit the game
+     * **signal**: quitGameSignal
+     */
+    @RegisterFunction
+    fun _on_quit_game() {
+        GD.print("Quitting game")
+        getTree()?.quit()
+    }
 
-	private fun loadConfiguration(): GameConfig {
-		// Load configuration file, if it exists
-		val configFile = FileAccess.open("user://config.json", FileAccess.ModeFlags.READ)
-		val gameConfig = if (configFile != null) {
-			val configString = configFile.getAsText()
-			Json.decodeFromString(GameConfig.serializer(), configString)
-		} else {
-			GameConfig()
-		}
-		return gameConfig
-	}
+    private fun loadConfiguration(): GameConfig {
+        // Load configuration file, if it exists
+        val configFile = FileAccess.open("user://config.json", FileAccess.ModeFlags.READ)
+        val gameConfig = if (configFile != null) {
+            val configString = configFile.getAsText()
+            Json.decodeFromString(GameConfig.serializer(), configString)
+        } else {
+            GameConfig()
+        }
+        return gameConfig
+    }
 
 
-	@RegisterFunction
-	fun _on_continue_game() {
-		loadGame(gameConfig.lastSaveFile!!)
-	}
+    @RegisterFunction
+    fun _on_continue_game() {
+        loadGame(gameConfig.lastSaveFile!!)
+    }
 
-	@RegisterFunction
-	fun _on_load_button_pressed() {
-		loadFileDialog.visible = true
-	}
+    @RegisterFunction
+    fun _on_load_button_pressed() {
+        loadFileDialog.visible = true
+    }
 
-	@RegisterFunction
-	fun _on_load_file_selected(file: String) {
-		loadGame(file)
-	}
+    @RegisterFunction
+    fun _on_load_file_selected(file: String) {
+        loadGame(file)
+    }
 
-	@RegisterFunction
-	fun loadGame(gameSave: String) {
-		if (FileAccess.fileExists(gameSave)) {
-			// Load game
-			val jsonString = FileAccess.open(gameSave, FileAccess.ModeFlags.READ)!!.getAsText()
-			val gameData = Json.decodeFromString(GameState.serializer(), jsonString)
-			GD.print("Loaded $gameSave : ${gameData.stateToString()}")
-			val gameState = getTree()?.root?.getChild(0) as GameState
-			gameState.deepCopy(gameData)
-			getTree()?.changeSceneToFile("res://src/main/kuiper/screens/kuiper/game.tscn")
-		} else {
-			GD.printErr("Save file not found: $gameSave")
-		}
-		loadButton.disabled = true
-		continueButton.disabled = true
-		continueButton.tooltipText = ""
-	}
+    @RegisterFunction
+    fun loadGame(gameSave: String) {
+        if (FileAccess.fileExists(gameSave)) {
+            // Load game
+            val json = Json {
+                prettyPrint = true
+                encodeDefaults = true
+                allowStructuredMapKeys = true
+            }
+            val jsonString = FileAccess.open(gameSave, FileAccess.ModeFlags.READ)!!.getAsText()
+            val gameData = json.decodeFromString(GameState.serializer(), jsonString)
+            GD.print("Loaded $gameSave : ${gameData.stateToString()}")
+            val gameState = getTree()?.root?.getChild(0) as GameState
+            gameState.deepCopy(gameData)
+            getTree()?.changeSceneToFile("res://src/main/kuiper/screens/kuiper/game.tscn")
+        } else {
+            GD.printErr("Save file not found: $gameSave")
+        }
+        loadButton.disabled = true
+        continueButton.disabled = true
+        continueButton.tooltipText = ""
+    }
 
 }
